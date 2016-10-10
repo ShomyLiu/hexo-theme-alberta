5851d9997e1743af8fe215237012152607264c3d
shomy <18222333186@163.com>
2016-10-10 18:47:51 +0800

    add to-top button

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
 (function($){
- 
+
   // Mobile nav
   var $container = $('#container'),
     isMobileNavAnim = false,
@@ -27,5 +27,20 @@
     if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
     $container.removeClass('mobile-nav-on');
+  });
+
+  //to top
+
+  $(window).scroll(function() {
+      $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+  });
+  $("#rocket").click(function() {
+      $("#rocket").addClass("launch");
+      $("html, body").animate({
+          scrollTop: 0
+      }, 500, function() {
+          $("#rocket").removeClass("show launch");
+      });
+      return false;
   });
 })(jQuery);
